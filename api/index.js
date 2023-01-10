17c6ed865edeee9f2fc5663ccfe0c2629d8ebd93
Luiz Cláudio <danellaclaudioluiz@gmail.com>
2023-01-10 16:41:11 -0300

    creating connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root1234",
+    database: "test",
+})
+
+app.get("/", (req, res) => {
+    res.json("hello, this is the home")
+})
 
 app.listen(8800, () => {
     console.log('connected to backend');
